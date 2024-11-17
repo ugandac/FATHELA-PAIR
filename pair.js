0e084b8413c1763249916ec88e88ac5ccbfdfe48
🔸A͜͡R͜͡L͜͡O͜͡D͜͡R͜͡A͜͡G͜͡O͜͡N͜͡🔸 <arlodeadpool@gmail.com>
2024-11-17 07:46:38 +0300

    ༺𝙏𝙔𝙍𝘼𝙓𝙀𝙎༻

--- pair.js
+++ pair.js
@@ -58,18 +58,10 @@ router.get('/', async (req, res) => {
 
                let GIFTED_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
-┗━━━━━━━━━━━━━━━
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 DAVID CYRIL 𖥘
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ WhattsApp Channel = https://whatsapp.com/channel/0029VaNPPwR30LKQk437x51Q
 Please Follow My Support Channel
-Wanna talk to me?👉 https://t.me/deecee_x 👈
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ©*2024-2099 David Cyril*
 
 _Don't Forget To Give Star To My Repo_`
